48b82567d7b332b3730ab0cad7b8e1f852523c32
Sean Brewer <seabre986@gmail.com>
2015-05-24 17:19:41 -0400

    Change admin user creation in seeds.rb to use find_or_create_by.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(email: 'admin@example.com',
-            password: 'password',
-            password_confirmation: 'password',
-            admin: true)
+User.find_or_create_by(email: 'admin@example.com') do |user|
+  user.password = 'password'
+  user.password_confirmation = 'password'
+  user.admin = true
+end
