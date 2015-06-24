b08f0edec3eaa573856ee38be74b537bb3520143
Bobby Burden <bobby@brb3.org>
2015-06-24 19:52:20 -0400

    Add Ransack and search form. Searches job titles and company names

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150525173513) do
+ActiveRecord::Schema.define(version: 20150624235531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,9 @@ ActiveRecord::Schema.define(version: 20150525173513) do
     t.datetime "updated_at",                  null: false
     t.string   "company"
   end
+
+  add_index "job_postings", ["company"], name: "index_job_postings_on_company", using: :btree
+  add_index "job_postings", ["title"], name: "index_job_postings_on_title", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
